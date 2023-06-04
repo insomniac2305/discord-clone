8a9329759810fdd3566b0bb46dbbdc509e168a81
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-06-04 22:14:53 +0200

    Add registration form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,33 @@
-import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import { useState } from "react";
 import TextInput from "./TextInput";
 import LinkButton from "./LinkButton";
 import PrimaryButton from "./PrimaryButton";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   return (
     <>
-      <div className="flex h-full flex-col items-center bg-gray-800 px-4 py-6 text-white sm:h-auto sm:w-[30rem] sm:rounded">
+      <div className="flex h-full w-full flex-col items-center gap-5 bg-gray-800 px-4 py-6 text-gray-300 sm:h-auto sm:w-[30rem] sm:rounded sm:shadow-md">
         <Logo />
-        <h1 className="m-4 text-xl font-bold tracking-wide">Welcome back!</h1>
-        <TextInput label="E-Mail" type="email" value={mail} onChange={setMail} required={true} />
-        <div className="mb-5 w-full" aria-hidden></div>
-        <TextInput label="Password" type="password" value={password} onChange={setPassword} required={true} />
-        <p className="mb-5 mt-1 w-full text-left text-xs">
-          <LinkButton text="Forgot password?" />
-        </p>
-        <div className="mb-2 w-full">
-          <PrimaryButton text="Sign in" />
+        <h1 className="text-xl font-bold tracking-wide">Welcome back!</h1>
+        <TextInput label="E-Mail" type="email" name="email" value={mail} onChange={setMail} required={true} />
+        <div className="w-full">
+          <TextInput label="Password" type="password" name="password" value={password} onChange={setPassword} required={true} />
+          <p className="mt-1 w-full text-left text-xs">
+            <LinkButton text="Forgot password?" />
+          </p>
         </div>
-        <p className="mt-1 w-full text-left text-xs text-gray-500 tracking-wide">
-          Need an account? <LinkButton text="Register" />
-        </p>
+        <div className="w-full">
+          <PrimaryButton text="Sign in" />
+          <p className="mt-2 w-full text-left text-xs tracking-wide text-gray-500">
+            Need an account? <LinkButton text="Register" onClick={() => navigate("/register")} />
+          </p>
+        </div>
       </div>
     </>
   );
