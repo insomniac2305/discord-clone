656e7e2d6bfc7a5ad8008b55ed2978951e94adae
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-06-06 17:27:24 +0200

    Change props to deconstructed object

--- src/pages/UserAuth.js
+++ src/pages/UserAuth.js
@@ -3,15 +3,15 @@ import Register from "../components/Register.js";
 import { LOGIN, REGISTER } from "../util/Constants.js";
 import AuthBackground from "../components/AuthBackground.js";
 
-function UserAuth(props) {
+function UserAuth({ mode }) {
   return (
     <>
       <div className="absolute -bottom-0 -right-0 left-0 top-0 -z-10">
         <AuthBackground />
       </div>
       <div className="flex h-full w-full sm:items-center sm:justify-center">
-        {props.mode === LOGIN && <Login />}
-        {props.mode === REGISTER && <Register />}
+        {mode === LOGIN && <Login />}
+        {mode === REGISTER && <Register />}
       </div>
     </>
   );
