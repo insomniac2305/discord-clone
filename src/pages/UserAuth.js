fc84cda17a4a683b2bf8ad4189f61eacbe9fdbe6
insomniac2305 <p.doerner@outlook.com>
2023-06-09 17:06:13 +0200

    Remove unnecessary hook

--- src/pages/UserAuth.js
+++ src/pages/UserAuth.js
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { LOGIN, REGISTER } from "../util/Constants.js";
 import Login from "../components/Login.js";
 import Register from "../components/Register.js";
@@ -6,8 +5,6 @@ import AuthBackground from "../components/AuthBackground.js";
 import Modal from "../components/Modal.js";
 
 function UserAuth({ mode }) {
-  const navigate = useNavigate();
-
   return (
     <>
       <div className="absolute -bottom-0 -right-0 left-0 top-0 -z-10">
