ef93da5e29962f791d20e4aa44190003ae5b498c
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-06-10 12:16:18 +0200

    Deconstruct TextInput props

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,16 +1,16 @@
-function TextInput(props) {
+function TextInput({ name, label, required, type, value, onChange }) {
   return (
     <div className="flex w-full flex-col">
-      <label htmlFor={props.name} className="mb-2 text-xs font-bold uppercase tracking-wider text-gray-500">
-        {props.label} {props.required && <span className="text-red">*</span>}
+      <label htmlFor={name} className="mb-2 text-xs font-bold uppercase tracking-wider text-gray-500">
+        {label} {required && <span className="text-red">*</span>}
       </label>
       <input
-        type={props.type}
-        name={props.name}
-        id={props.name}
-        value={props.value}
-        required={props.required}
-        onChange={(e) => props.onChange(e.target.value)}
+        type={type}
+        name={name}
+        id={name}
+        value={value}
+        required={required}
+        onChange={(e) => onChange(e.target.value)}
         className="rounded bg-gray-900 p-2"
       />
     </div>
