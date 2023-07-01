b6dfc1f35241df0213105ef376db116f9b1208dd
insomniac2305 <p.doerner@outlook.com>
2023-07-01 16:33:42 +0200

    Add new message form

--- src/components/ChannelContent.jsx
+++ src/components/ChannelContent.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import ChannelMembers from "./ChannelMembers";
+import NewMessage from "./NewMessage";
 
 function ChannelContent({ isMembersVisible }) {
   return (
     <div className="relative flex h-full w-full overflow-hidden">
       <div
         className={
-          "w-full transition-all p-1 " +
+          "flex w-full flex-col p-1 transition-all " +
           (isMembersVisible ? "-translate-x-[15rem] md:w-[calc(100%-15rem)] md:-translate-x-0" : "-translate-x-[0]")
         }
       >
-        <div className="h-full overflow-y-auto overflow-x-hidden">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden">
           <ol className="flex flex-col">
             <li>Message 1</li>
             <li>Message 2</li>
             <li>Message 3</li>
           </ol>
         </div>
+        <NewMessage channelName={"general"} />
       </div>
       <ChannelMembers isVisible={isMembersVisible} />
     </div>
