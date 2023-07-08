da8b23f3cfa474026ba437456f990ee6627c3081
insomniac2305 <p.doerner@outlook.com>
2023-07-08 21:48:34 +0200

    Add edit icon to chosen image

--- src/components/IconPicker.jsx
+++ src/components/IconPicker.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ImageUploadIcon from "./ImageUploadIcon";
+import { BiSolidPencil } from "react-icons/bi";
 
 function IconPicker({ onChange, initialIconUrl }) {
   const [iconSelection, setIconSelection] = useState(null);
-  const [iconUrl, setIconUrl] = useState(initialIconUrl);
+  const [iconUrl, setIconUrl] = useState(null);
   const [isPlaceholderVisible, setIsPlaceholderVisible] = useState(true);
 
   useEffect(() => {
     setIsPlaceholderVisible(!iconSelection && !initialIconUrl);
   }, [iconSelection, initialIconUrl]);
-  
+
   const onIconSelection = (e) => {
     if (!e.target.files || e.target.files.length === 0) {
       setIconSelection(null);
@@ -24,7 +25,7 @@ function IconPicker({ onChange, initialIconUrl }) {
 
   useEffect(() => {
     if (!iconSelection) {
-      setIconUrl(null);
+      setIconUrl(initialIconUrl);
       return;
     }
 
@@ -37,7 +38,14 @@ function IconPicker({ onChange, initialIconUrl }) {
   return (
     <div className="relative">
       {isPlaceholderVisible && <ImageUploadIcon />}
-      {!isPlaceholderVisible && <img src={iconUrl} alt="Picked Icon" className="h-20 w-20 rounded-full object-cover" />}
+      {!isPlaceholderVisible && (
+        <>
+          <img src={iconUrl} alt="Picked Icon" className="h-20 w-20 rounded-full object-cover" />
+          <span className="absolute right-0 top-0 rounded-full bg-blurple-400 p-1 text-lg text-white">
+            <BiSolidPencil />
+          </span>
+        </>
+      )}
       <input
         className="absolute left-0 top-0 h-full w-full opacity-0"
         type="file"
