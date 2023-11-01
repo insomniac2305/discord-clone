422deca05d054c570392397d214242a2f034ee52
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-11-01 19:46:55 +0100

    Implement member list request with new backend

--- src/pages/Main/ChannelMembers.jsx
+++ src/pages/Main/ChannelMembers.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import useServerMembers from "../../hooks/useServerMembers";
+import React, { useContext, useEffect } from "react";
 import User from "../../components/User";
 import { ROLE_ADMIN } from "../../util/Constants";
 import { FaCrown } from "react-icons/fa";
+import useBackendRequest from "../../hooks/useBackendRequest";
+import AuthContext from "../../util/AuthContext";
 
 function ChannelMembers({ isVisible, serverId }) {
-  const [serverMembers] = useServerMembers(serverId);
+  const { token } = useContext(AuthContext);
+  const [requestServerMembers, serverMembers] = useBackendRequest(serverId && `api/servers/${serverId}/members`);
+
+  useEffect(() => {
+    if (token && serverId) {
+      requestServerMembers(token);
+    }
+  }, [token, serverId]);
 
   return (
     <div
@@ -17,8 +25,8 @@ function ChannelMembers({ isVisible, serverId }) {
       <ul className="mx-2 my-4">
         {serverMembers?.map((member) => {
           return (
-            <li className="flex items-center gap-2 rounded p-1 hover:bg-gray-680" key={member.id}>
-              <User username={member.username} avatarUrl={member.avatarUrl} />
+            <li className="flex items-center gap-2 rounded p-1 hover:bg-gray-680" key={member.user?._id}>
+              <User username={member.user?.name} avatarUrl={member.user?.avatar} />
               {member.role === ROLE_ADMIN && <FaCrown className="text-sm text-[#f0b132]" />}
             </li>
           );
